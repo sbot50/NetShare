1cae141ede0327f09aa7aa4bf37ff5e9457d57dd
sbot50 <abc@def>
2025-11-29 16:36:56 +0100

    Highlighted Join instead of Host Made the Stop Streaming and Disconnect button smaller and less red. Made the ID not be prefilled and disabled the button if its empty on client Added a copy button to the id at the top of the host screen

--- client/scripts/load/id.js
+++
@@ -1,9 +0,0 @@
-import init from "../init/id.js"; //Dependency
-init.then(() => {
-    document.querySelector("#id").onchange = () => {
-        if (document.querySelector("#id").value === "") {
-            document.querySelector("#id").value = Math.random().toFixed(6).substring(2);
-        }
-        localStorage.setItem("clientID", document.querySelector("#id").value);
-    };
-});
